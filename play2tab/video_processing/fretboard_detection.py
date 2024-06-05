531de499a7d4650ef3ced9f5d34a3ac6799385b4
HypocriteLecteur <73636031+HypocriteLecteur@users.noreply.github.com>
2024-06-05 21:50:28 +0800

    added homography rectification

--- play2tab/video_processing/fretboard_detection.py
+++ play2tab/video_processing/fretboard_detection.py
@@ -5,6 +5,8 @@ from itertools import combinations
 from utils.utils import angle_diff
 from scipy.stats import circmean
 
+fret_length_scaling = 2**(1/12) / (2**(1/12) - 1)
+
 def rotate_image(image, image_center, angle):
   rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)  # deg
   result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
@@ -73,31 +75,80 @@ if __name__ == '__main__':
 
         rect = cv2.minAreaRect(cornerpoints)
         if rect[1][1] > rect[1][0]:
-            rotated = rotate_image(edges, rect[0], -(90-rect[2]))
+            rotated = rotate_image(frame, rect[0], -(90-rect[2]))
+            mask = np.zeros_like(frame)
+            mask = cv2.rectangle(mask, (0,int(rect[0][1]-0.6*rect[1][0])),(w,int(rect[0][1]+0.6*rect[1][0])), 255, -1)
+            rotated = rotated[int(rect[0][1]-0.6*rect[1][0]):int(rect[0][1]+0.6*rect[1][0]), :]
+            # rotated = cv2.bitwise_and(rotated, mask)
         else:
-            rotated = rotate_image(edges, rect[0], rect[2])
-        cv2.imshow('rotated', rotated)
-        cv2.waitKey(1)
+            rotated = rotate_image(frame, rect[0], rect[2])
+            mask = np.zeros_like(frame)
+            mask = cv2.rectangle(mask, (0,int(rect[0][1]-0.6*rect[1][1])),(w,int(rect[0][1]+0.6*rect[1][1])), 255, -1)
+            rotated = rotated[int(rect[0][1]-0.6*rect[1][1]):int(rect[0][1]+0.6*rect[1][1]), :]
+            # rotated = cv2.bitwise_and(rotated, mask)
+        rotated = cv2.equalizeHist(rotated)
 
+        rotated_edges = cv2.Canny(rotated, 50, 150, 5)
 
         # Second stage: fret detection
-        fret_theta = circmean([filtered_parallel_lines[0, 2], filtered_parallel_lines[0, 4]], low=-np.pi/2, high=np.pi/2)
-        fret_theta = fret_theta + np.pi/2
-        if fret_theta >= np.pi / 2:
-            fret_theta = fret_theta - np.pi
+        # fret_theta = circmean([filtered_parallel_lines[0, 2], filtered_parallel_lines[0, 4]], low=-np.pi/2, high=np.pi/2)
+        # fret_theta = fret_theta + np.pi/2
+        # if fret_theta >= np.pi / 2:
+        #     fret_theta = fret_theta - np.pi
         
+        # theta_range = 10/180*np.pi
+        # theta_tol = 5  # degree
+        # rho_tol = 10  # pixel
+        # tested_angles = np.linspace(fret_theta-theta_range, fret_theta+theta_range, 10, endpoint=False)
+        # h, theta, d = hough_line(edges, theta=tested_angles)
+        # hspace, angles, dists = hough_line_peaks(h, theta, d, min_distance=9, min_angle=10)
+    
         theta_range = 10/180*np.pi
         theta_tol = 5  # degree
         rho_tol = 10  # pixel
-        tested_angles = np.linspace(fret_theta-theta_range, fret_theta+theta_range, 10, endpoint=False)
-        h, theta, d = hough_line(edges, theta=tested_angles)
+        tested_angles = np.linspace(-theta_range, theta_range, 10, endpoint=False)
+        h, theta, d = hough_line(rotated_edges, theta=tested_angles)
         hspace, angles, dists = hough_line_peaks(h, theta, d, min_distance=9, min_angle=10)
 
-        cdst2 = cv2.cvtColor(edges.copy(), cv2.COLOR_GRAY2BGR)
+        # cdst2 = cv2.cvtColor(rotated_edges.copy(), cv2.COLOR_GRAY2BGR)
+        # for angle, dist in zip(angles, dists):
+        #     draw_line(cdst2, dist, angle)
+
+        # sort by dist
+        angles, dists = (list(item) for item in zip(*sorted(zip(angles, dists), key=lambda x: x[1])))
+        
+        src_pts = np.empty((0, 2))
+        dst_pts = np.empty((0, 2))
+        for angle, dist in zip(angles, dists):
+            tmp_src_pts = np.array([
+                [dist / np.cos(angle), 0],
+                [(dist - 200*np.sin(angle)) / np.cos(angle), 200]
+            ])
+            tmp_dst_pts = np.array([
+                [dist / np.cos(angle), 0],
+                [dist / np.cos(angle), 200]
+            ])
+            src_pts = np.concatenate((src_pts, tmp_src_pts), axis=0)
+            dst_pts = np.concatenate((dst_pts, tmp_dst_pts), axis=0)
+        M, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+
+        cdst2 = cv2.cvtColor(rotated_edges.copy(), cv2.COLOR_GRAY2BGR)
         for angle, dist in zip(angles, dists):
             draw_line(cdst2, dist, angle)
+
+        cdst2 = cv2.warpPerspective(cdst2, M, rotated_edges.shape[1::-1], flags=cv2.INTER_LINEAR)
+
+        # theta_range = 10/180*np.pi
+        # theta_tol = 5  # degree
+        # rho_tol = 10  # pixel
+        # tested_angles = np.linspace(-theta_range, theta_range, 10, endpoint=False)
+        # h, theta, d = hough_line(rotated_edges, theta=tested_angles)
+        # hspace, angles, dists = hough_line_peaks(h, theta, d, min_distance=9, min_angle=10)
         
-        cv2.imshow('frame', frame)
-        cv2.imshow('direction', cdst)
+        
+        # cv2.imshow('rotated', result)
+        # cv2.waitKey(1)
+        
+        # cv2.imshow('direction', cdst)
         cv2.imshow('frets', cdst2)
-        cv2.waitKey(1)
+        cv2.waitKey()
