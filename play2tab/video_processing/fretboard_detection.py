c1692411b34782f98d1d6d17abad28c4e73f39b7
HypocriteLecteur <73636031+HypocriteLecteur@users.noreply.github.com>
2024-06-05 17:13:48 +0800

    added principal axis detection

---
+++ play2tab/video_processing/fretboard_detection.py
@@ -0,0 +1,103 @@
+import cv2
+import numpy as np
+from skimage.transform import hough_line, hough_line_peaks
+from itertools import combinations
+from utils.utils import angle_diff
+from scipy.stats import circmean
+
+def rotate_image(image, image_center, angle):
+  rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)  # deg
+  result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+  return result
+
+def draw_line(image, rho, theta):
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a * rho
+    y0 = b * rho
+    pt1 = (int(x0 + 2000*(-b)), int(y0 + 2000*(a)))
+    pt2 = (int(x0 - 2000*(-b)), int(y0 - 2000*(a)))
+    cv2.line(image, pt1, pt2, (0,0,255), 1, cv2.LINE_AA)
+
+if __name__ == '__main__':
+    frame_number = 500
+    cap = cv2.VideoCapture('test\\guitar.mp4')
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            ret, frame = cap.read()
+        
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # First stage: guitar direction detection
+        lower_threshold = 30
+        high_threshold = 100
+        aperture_size = 5
+        is_L2gradient = True
+        edges = cv2.Canny(frame, lower_threshold, high_threshold, aperture_size)
+        # edges = cv2.Sobel(frame, cv2.CV_8U, 1, 0, 3)
+
+        theta_tol = 5  # degree
+        rho_tol = 100  # pixel
+        tested_angles = np.linspace(-np.pi / 2, np.pi / 2, 360, endpoint=False)
+        h, theta, d = hough_line(edges, theta=tested_angles)
+        hspace, angles, dists = hough_line_peaks(h, theta, d, min_distance=9, min_angle=10, threshold=100)
+        parallel_lines = []
+        for [[hi, ri, ti], [hj, rj, tj]] in combinations(zip(hspace, dists, angles), 2):
+            if angle_diff(ti, tj) < theta_tol / 180 * np.pi and abs(ri - rj) > rho_tol:
+                parallel_lines.append([hi+hj, ri, ti, rj, tj])
+        parallel_lines = np.array(parallel_lines)
+
+        filtered_parallel_lines = parallel_lines[parallel_lines[:, 0] >= 1*np.max(parallel_lines[:, 0]), :]
+
+        cdst = cv2.cvtColor(edges.copy(), cv2.COLOR_GRAY2BGR)
+        for line_pair in filtered_parallel_lines:
+            draw_line(cdst, line_pair[1], line_pair[2])
+            draw_line(cdst, line_pair[3], line_pair[4])
+        
+        fretboard_linepair = filtered_parallel_lines[0]
+        w = frame.shape[1]
+        r1 = fretboard_linepair[1]
+        t1 = fretboard_linepair[2]
+        r2 = fretboard_linepair[3]
+        t2 = fretboard_linepair[4]
+
+        cornerpoints = np.array([[
+            [0, int(r1/np.sin(t1))],
+            [w, int((r1-w*np.cos(t1))/np.sin(t1))],
+            [0, int(r2/np.sin(t2))],
+            [w, int((r2-w*np.cos(t2))/np.sin(t2))]
+        ]])
+
+        rect = cv2.minAreaRect(cornerpoints)
+        if rect[1][1] > rect[1][0]:
+            rotated = rotate_image(edges, rect[0], -(90-rect[2]))
+        else:
+            rotated = rotate_image(edges, rect[0], rect[2])
+        cv2.imshow('rotated', rotated)
+        cv2.waitKey(1)
+
+
+        # Second stage: fret detection
+        fret_theta = circmean([filtered_parallel_lines[0, 2], filtered_parallel_lines[0, 4]], low=-np.pi/2, high=np.pi/2)
+        fret_theta = fret_theta + np.pi/2
+        if fret_theta >= np.pi / 2:
+            fret_theta = fret_theta - np.pi
+        
+        theta_range = 10/180*np.pi
+        theta_tol = 5  # degree
+        rho_tol = 10  # pixel
+        tested_angles = np.linspace(fret_theta-theta_range, fret_theta+theta_range, 10, endpoint=False)
+        h, theta, d = hough_line(edges, theta=tested_angles)
+        hspace, angles, dists = hough_line_peaks(h, theta, d, min_distance=9, min_angle=10)
+
+        cdst2 = cv2.cvtColor(edges.copy(), cv2.COLOR_GRAY2BGR)
+        for angle, dist in zip(angles, dists):
+            draw_line(cdst2, dist, angle)
+        
+        cv2.imshow('frame', frame)
+        cv2.imshow('direction', cdst)
+        cv2.imshow('frets', cdst2)
+        cv2.waitKey(1)
--- play2tab/video_processing/utils/utils.py
+++ play2tab/video_processing/utils/utils.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 
 def angle_diff(ang1, ang2):
@@ -6,3 +7,44 @@ def angle_diff(ang1, ang2):
         return np.pi - diff
     else:
         return diff
+
+def nothing(x):
+    pass
+
+class ColorThresholder:
+    def __init__(self):
+        cv2.namedWindow('color thresholder')
+
+        cv2.createTrackbar('HMin','color thresholder',0,179,nothing) # Hue is from 0-179 for Opencv
+        cv2.createTrackbar('SMin','color thresholder',0,255,nothing)
+        cv2.createTrackbar('VMin','color thresholder',0,255,nothing)
+        cv2.createTrackbar('HMax','color thresholder',0,179,nothing)
+        cv2.createTrackbar('SMax','color thresholder',0,255,nothing)
+        cv2.createTrackbar('VMax','color thresholder',0,255,nothing)
+
+        # Set default value for MAX HSV trackbars.
+        cv2.setTrackbarPos('HMax', 'color thresholder', 179)
+        cv2.setTrackbarPos('SMax', 'color thresholder', 255)
+        cv2.setTrackbarPos('VMax', 'color thresholder', 255)
+
+        self.hMin = self.sMin = self.vMin = self.hMax = self.sMax = self.vMax = 0
+    
+    def update(self, image):
+        self.hMin = cv2.getTrackbarPos('HMin','color thresholder')
+        self.sMin = cv2.getTrackbarPos('SMin','color thresholder')
+        self.vMin = cv2.getTrackbarPos('VMin','color thresholder')
+
+        self.hMax = cv2.getTrackbarPos('HMax','color thresholder')
+        self.sMax = cv2.getTrackbarPos('SMax','color thresholder')
+        self.vMax = cv2.getTrackbarPos('VMax','color thresholder')
+
+        # Set minimum and max HSV values to display
+        lower = np.array([self.hMin, self.sMin, self.vMin])
+        upper = np.array([self.hMax, self.sMax, self.vMax])
+
+        # Create HSV Image and threshold into a range.
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, lower, upper)
+        output = cv2.bitwise_and(image, image, mask= mask)
+
+        cv2.imshow('color thresholder', output)
